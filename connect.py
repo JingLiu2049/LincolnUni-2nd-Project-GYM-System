ae8ac38a18bba75643a7d7d3bfb659a05123fcd6
Chiyu He <Finn.He@lincolnuni.ac.nz>
2021-04-02 16:05:18 +1300

    finn

--- connect.py
+++ connect.py
@@ -3,3 +3,10 @@ dbpass = "comp639!" #"PUT YOUR PASSWORD HERE"
 dbhost = "comp639.cldqh6d4e9ir.us-east-1.rds.amazonaws.com" #"PUT YOUR AWS Connect String here"
 dbport = "5432"
 dbname = "comp639"
+
+
+# dbuser = "postgres"
+# dbpass = "xxxxxxxxxx" #"PUT YOUR PASSWORD HERE"
+# dbhost = "localhost" #"PUT YOUR AWS Connect String here"
+# dbport = "5432"
+# dbname = "test_group"
